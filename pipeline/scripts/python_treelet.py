1573fa7e57b531240d283fa38190ab1b05c39480
Shakeel95 <shakeel.gavioli@googlemail.com>
2019-12-18 19:00:29 +0000

    minor changes - BROKEN

--- pipeline/scripts/python_treelet.py
+++ pipeline/scripts/python_treelet.py
@@ -21,8 +21,8 @@ def jacobi_rotation(C,a,b):
     cos_theta = np.cos(theta)
     sine_theta = np.sin(theta)
     
-    rotation = np.array([[cos_theta, sine_theta],
-                  [-sine_theta, cos_theta]])
+    rotation = np.array([[cos_theta, -sine_theta],
+                  [sine_theta, cos_theta]])
     
     return rotation
 
@@ -38,16 +38,10 @@ def rotate_covariance_matrix(C, a, b, J):
     """
     
     
-#     p = C.shape[0]
-#     R = np.identity(p)
-#     R[a,a], R[a,b], R[b,a], R[b,b] = J.flatten()
-    
-#     C = np.matmul(np.matmul(R.transpose(),C),R)
-
-    M = C 
-    M[[a,b],:] = np.matmul(J.transpose(),C[[a,b],:])
-    C = M 
-    C[:,[a,b]] = np.matmul(M[:,[a,b]],J)
+    p = C.shape[0]
+    R = np.identity(p)
+    R[a,a], R[a,b], R[b,a], R[b,b] = J.flatten()
+    C = np.matmul(np.matmul(R.transpose(),C),R)
     
     return C
 
@@ -98,44 +92,7 @@ def treelet_decomposition(X, L):
                       "order": (1,0) if C[a,a] > C[b,b] else (0,1)
                      }
         
-        mask += [b if C[a,a] < C[b,b] else a]
+        mask += [b if C[a,a] > C[b,b] else a]
         print(mask)
-        
-    return treelet
-
-
-def treelet_decomposition1(C, L): 
-
-    treelet = {0:{"C": C, 
-                  "J": None,
-                  "pair": (None, None), 
-                  "order": (None, None)}
-              }
-    mask = []
-    
-    for l in range(1,L):
-        
-        # find most correlated vars 
-        which_max = np.abs(np.triu(C, +1))
-        if (l > 1):
-            which_max[mask,:] = -1
-            which_max[:,mask] = -1    
-        a, b = np.unravel_index(np.argmax(which_max, axis = None), which_max.shape)
-        print((a,b))
-        
-        # calc. Jacobi rotation 
-        J = jacobi_rotation(C, a, b)
-        
-        # update correlation matrix
-        C = rotate_covariance_matrix(C,a,b,J)
-        
-        # update treelet
-        treelet[l] = {"C": C,
-                      "J": J,
-                      "pair": (a,b), 
-                      "order": (1,0) if C[a,a] > C[b,b] else (0,1)
-                     }
-        
-        mask += [b if C[a,a] < C[b,b] else a]
         
     return treelet
